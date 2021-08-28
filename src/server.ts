184ad05f9911dce107d54a82ba1be1cc2dabec1f
WendelSantosD <wendelwcsantos@gmail.com>
2021-08-28 19:03:42 -0300

    debugando a aplicação

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,18 @@
-import { Express } from 'express'
+import express from 'express'
+
+const app = express()
+
+console.clear()
+
+app.use(express.json())
+app.post('/courses', (req, res) => {
+    const { name } = req.body
+
+    res.json({ name })
+})
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Hello World' })
+})
+
+app.listen(3333, () => console.log('Server is running!'))
