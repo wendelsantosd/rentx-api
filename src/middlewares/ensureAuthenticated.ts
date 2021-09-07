e74bb57ddfd120b2da87b367a5825573c0db4005
WendelSantosD <wendelwcsantos@gmail.com>
2021-09-07 19:16:34 -0300

    implements middleware

---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,40 @@
+import { Request, Response, NextFunction } from 'express'
+import { verify } from 'jsonwebtoken'
+
+import { UsersRepository } from '../modules/accounts/repositories/implementations/UsersRepository'
+
+interface IPayload {
+    sub: string
+}
+
+export async function ensureAuthenticated(
+    req: Request,
+    res: Response,
+    next: NextFunction
+): Promise<void> {
+    const authHeader = req.headers.authorization
+
+    if (!authHeader) {
+        throw new Error('Token missing')
+    }
+
+    const [, token] = authHeader.split(' ')
+
+    try {
+        const { sub: user_id } = verify(
+            token,
+            'fc5e038d38a57032085441e7fe7010b0'
+        ) as IPayload
+
+        const usersRepository = new UsersRepository()
+        const user = usersRepository.findById(user_id)
+
+        if (!user) {
+            throw new Error('User does not exists')
+        }
+
+        next()
+    } catch {
+        throw new Error('invalid token')
+    }
+}
