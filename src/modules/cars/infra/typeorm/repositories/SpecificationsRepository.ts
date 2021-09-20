2da2f6539c8b862afcfde3d119476f5e5593d5b8
WendelSantosD <wendelwcsantos@gmail.com>
2021-09-20 10:23:37 -0300

    refatora

--- src/modules/cars/infra/typeorm/repositories/SpecificationsRepository.ts
+++ src/modules/cars/infra/typeorm/repositories/SpecificationsRepository.ts
@@ -1,10 +1,8 @@
+import { ICreateSpecificationDTO, ISpecificationsRepository } from '@modules/cars/repositories/ISpecificationsRepository'
 import { getRepository, Repository } from 'typeorm'
+import { Specification } from '../entities/Specification'
 
-import { Specification } from '../../entities/Specification'
-import {
-    ISpecificationsRepository,
-    ICreateSpecificationDTO,
-} from '../ISpecificationsRepository'
+
 
 class SpecificationsRepository implements ISpecificationsRepository {
     private repository: Repository<Specification>
